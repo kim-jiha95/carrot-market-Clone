55f56ff159edde3aaf521db41f88c196dc0749f2
kim-jiha95 <kimjiha1112@gmail.com>
2021-04-05 01:12:23 +0900

    header bar plus

--- App.jsx
+++ App.jsx
@@ -1,15 +1,39 @@
 import React, { useState, useEffect } from 'react';
 import { StyleSheet } from 'react-native';
 
+import { createStackNavigator} from "@react-navigation/stack";
 import { NavigationContainer } from '@react-navigation/native';
 import StackNavigator from './navigations/StackNavigator';
+
+// const Stack = createStackNavigator();
 import TabNavigator from './navigations/TabNavigator';
+// import "React-native-gesture-handler";
+// import {HomePage, HoodPage, ChatPage, MyPage} from '../pages'
+import HoodPage from "./pages/HoodPage";
+import HomePage from "./pages/HomePage";
+import ChatPage from "./pages/ChatPage";
+import MyPage from "./pages/MyPage";
+
 
 export default function App() {
+  const Stack = createStackNavigator();
   return (
     <NavigationContainer>
-      <StackNavigator />
-    </NavigationContainer>
+    <StackNavigator />
+  </NavigationContainer>
+    // <NavigationContainer>
+    //   <Stack.Navigator 
+    //     screenOptions={{
+    //       headerShown: false
+    //     }}
+    //     initialRouteName={"Home"}
+    //     >
+    //       <Stack.Screen name ="HomePage" component={HomePage}/>
+    //       <Stack.Screen name ="HoodPage" component={HoodPage}/>
+    //       <Stack.Screen name ="ChatPage" component={ChatPage}/>
+    //       <Stack.Screen name ="MyPage" component={MyPage}/>
+    //       </Stack.Navigator>         
+    // </NavigationContainer>
   );
 }
 
--- pages/HoodPage.jsx
+++ pages/HoodPage.jsx
@@ -1,26 +1,112 @@
 import React from 'react';
-import { StyleSheet, View, Text, Image } from 'react-native';
+import { SafeAreaView,StyleSheet, View, Text, Image, TouchableOpacity, FlatList } from 'react-native';
 
+import {icons, images, SIZES, COLORS, FONTS} from '../constants'
 
-export default function Mypage() {
+const HoodPage = () => {
+
+  // 더미데이터 여기에 put
+  
+  function renderHeader() {
+    return (
+      <View style={{ flexDirection: 'row', height:50}}>
+        <TouchableOpacity
+          style={{
+            width: 50,
+            paddingLeft: SIZES.padding *2,
+            justifyConetent: 'center'
+          }}
+        >
+          <Image
+              source={icons.nearby}
+              resizeMode="contain"
+              style={{
+                width:30,
+                height:30
+              }}
+          />
+
+        </TouchableOpacity>
+
+        <View style={{ flex: 1, aligitems: 'center', justifyConetent: 'center'}}>
+                <View 
+                style={{
+                    width: '70%',
+                    height: "100%",
+                    backgroundColor:COLORS.lightGray3,
+                    alignItems: 'center',
+                    justifyContent:'center',
+                    borderRadius: SIZES.radius
+                }}>
+                  <Text style={{ ...FONTS.h3}}>잠실4동</Text>
+                </View>
+        </View>
+
+        <TouchableOpacity
+              style={{
+                width:50,
+                paddingRight:SIZES.padding * 2,
+                justifyContent: 'center'
+              }}
+        >
+              <Image
+                  source={icons.basket}
+                  resizeMode="contain"
+                  style={{
+                    width:30,
+                    height:30
+                  }}
+              />
+          
+        </TouchableOpacity>
+      </View>
+    )
+  }
+  
   return (
-    <View>      
-      <Image style ={styles.plus}         
-         source={require("../assets/plus.png")}
-/>
-      
-    </View>
-  );
+    <SafeAreaView style={styles.container}>
+      {renderHeader()}
+    </SafeAreaView>
+  )
 }
+// export default function HoodPage() {
+//   return (
+//     <View>      
+//       <Image style ={styles.plus}         
+//          source={require("../assets/plus.png")}
+// />
+      
+//     </View>
+//   );
+// }
+
+
 
 const styles = StyleSheet.create({
-    plus:{
-      marginTop: 680,
-      marginLeft: 310,
-      width: 50,
-      height: 50,
-    }
+    // plus:{
+    //   marginTop: 680,
+    //   marginLeft: 310,
+    //   width: 50,
+    //   height: 50,
+    // },
+    container: {
+      flex:1,
+      backgroundColor: COLORS.lightGray4
+    },
+    // Shadow: {
+    //   ShadowColor: "#000",
+    //   ShadowOffset: {
+    //     width:0,
+    //     height: 3,
+    //   },
+    //   shadowOpacity: 0.1,
+    //   shadowRadius: 3,
+    //   elevation: 1,
+    // }
   
  
   
-});
+})
+
+export default HoodPage;
+
