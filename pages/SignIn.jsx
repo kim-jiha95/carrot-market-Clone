fbbfdca79a689b982c639c64b42702dd7a9ef021
deathsizes <deathsizes@hotmail.com>
2021-04-05 14:34:26 +0900

    commit on main

--- pages/SignIn.jsx
+++ pages/SignIn.jsx
@@ -1,18 +1,148 @@
-import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, ImageBackground } from 'react-native';
+import ItemInput from '../components/ItemInput';
+import {
+  Container,
+  Content,
+  Text,
+  Form,
+  Button,
+} from 'native-base';
+const bImage = require('../assets/background.png');
 
-export default function SignInPage() {
+export default function SignIn({ navigation }) {
+
+  
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const [emailError, setEmailError] = useState('');
+  const [passwordError, setPasswordError] = useState('');
+
+  const goSignUp = () => {
+    navigation.navigate('SignUp');
+  };
+
+  const doSignIn = () => {
+    //Email 로그인 버튼을 누를 때 실행
+    //관리 상태 값 확인
+    console.log(email);
+    console.log(password);
+    if (email == '') {
+      setEmailError('이메일을 입력해주세요');
+    } else {
+      setEmailError('');
+    }
+
+    if (password == '') {
+      setPasswordError('비밀번호를 입력해주세요');
+    } else {
+      setPasswordError('');
+    }
+    // signIn(email, password, navigation);
+    //header삭제
+  };
+  const setEmailFunc = (itemInputEmail) => {
+    setEmail(itemInputEmail);
+  };
+  const setPasswordFunc = (itemInputPassword) => {
+    setPassword(itemInputPassword);
+  };
+
+
   return (
-    <View style={styles.container}>
-      <Text>caroot ChatPage!</Text>
-    </View>
+    <Container style={styles.container}>
+      <ImageBackground source={bImage} style={styles.backgroundImage}>
+        <Content contentContainerStyle={styles.content} scrollEnabled={false}>
+          <Text style={styles.title}>
+            <Text style={styles.highlite}>당근</Text>마켓
+          </Text>
+          <Form style={styles.form}>
+            <ItemInput title={'이메일'} 
+            type={'email'} 
+            setFunc={setEmailFunc}
+            error={emailError} 
+            />
+            <ItemInput title={'비밀번호'} 
+            type={'password'}
+            setFunc={setPasswordFunc}
+            error={passwordError} />
+          </Form>
+          
+          
+          <Button full style={styles.emailSignIn} onPress={doSignIn}>
+            <Text>Email 로그인</Text>
+          </Button>
+          <Button full style={styles.emailSignUp} onPress={goSignUp}>
+            <Text style={{ color: '#333' }}>회원가입</Text>
+          </Button>
+        </Content>
+      </ImageBackground>
+    </Container>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  container: {},
+  backgroundImage: {
+    width: '100%',
+    height: '100%',
+  },
+  content: {
     flex: 1,
-    justifyContent: 'center',
     alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'rgba(52, 52, 52, 0.5)',
+    margin: 20,
+    borderRadius: 20,
+  },
+  title: {
+    fontSize: 25,
+    fontWeight: '700',
+    color: '#fff',
+    textAlign: 'center',
+  },
+  highlite: {
+    fontSize: 25,
+    fontWeight: '700',
+    color: '#df3f32',
+    textAlign: 'center',
+  },
+  form: {
+    width: 250,
+    borderRadius: 10,
+    paddingBottom: 20,
+    paddingRight: 20,
+    paddingLeft: 20,
+    marginTop: 10,
+  },
+  label: {
+    color: '#fff',
+  },
+  input: {
+    color: '#fff',
+  },
+  snsSignUp: {
+    alignSelf: 'center',
+    width: 250,
+    marginTop: 10,
+    borderRadius: 10,
+    backgroundColor: '#4667A5',
+  },
+  emailSignIn: {
+    alignSelf: 'center',
+    width: 250,
+    marginTop: 5,
+    borderRadius: 10,
+    backgroundColor: '#333',
+  },
+  emailSignUp: {
+    alignSelf: 'center',
+    width: 250,
+    marginTop: 5,
+    borderRadius: 10,
+    backgroundColor: '#eee',
+    borderWidth: 1,
+    borderColor: '#333',
   },
 });
